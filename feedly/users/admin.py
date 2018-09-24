a334dde8ccc05815828045aa11e99980975c3355
Pramod-Jaiswal <pramodjaiswal517@gmail.com>
2018-09-24 09:37:44 +0530

    post by user is created voting/comment remaining

--- feedly/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MyProfile
-from .models import Post,Vote
-
-admin.site.register(Post)
-admin.site.register(Vote)
-admin.site.register(MyProfile)
