248759ea6aae1a3714d674230eedfc0e32d54021
mr-star-k <pramodjaiswal517@gmail.com>
2019-06-13 16:41:09 +0530

    changed in whitenoise version

--- feedly/settings.py
+++ feedly/settings.py
@@ -17,6 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
+
+
+
+
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
