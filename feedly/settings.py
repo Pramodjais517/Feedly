aac1a3eeb60fcdf22600e7c872772d1cfea7d211
mr-star-k <pramodjaiswal517@gmail.com>
2019-06-13 19:33:52 +0530

    updated for heroku

--- feedly/settings.py
+++ feedly/settings.py
@@ -168,3 +168,10 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, "static")
  ]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
