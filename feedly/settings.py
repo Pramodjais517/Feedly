443279823b195da160da038d732d4afe2c2d0e56
mr-star-k <pramodjaiswal517@gmail.com>
2019-06-13 16:11:07 +0530

    changed in whitenoise version

--- feedly/settings.py
+++ feedly/settings.py
@@ -151,7 +151,7 @@ USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
